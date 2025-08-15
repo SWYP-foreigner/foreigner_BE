8c3a94be13abfaed3e0a9780907bb6171b34c35c
usingjun <usingjun99@gmail.com>
2025-08-15 21:51:25 +0900

    feature: 이미지 엔티티

--- src/main/java/core/global/image/entity/Image.java
+++ src/main/java/core/global/image/entity/Image.java
@@ -1,13 +1,15 @@
 package core.global.image.entity;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "image")
 @Getter
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Image {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -15,11 +17,35 @@ public class Image {
     private Long id;
 
     @Column(name = "image_type", nullable = false)
-    private String imageType;
+    private ImageType imageType;
 
     @Column(name = "related_id", nullable = false)
     private Long relatedId;
 
     @Column(name = "url", length = 150, nullable = false)
     private String url;
+
+    @Column(name = "position", nullable = false)
+    private Integer position;
+
+    @Builder
+    private Image(ImageType imageType, Long relatedId, String url, Integer position) {
+        this.imageType = imageType;
+        this.relatedId = relatedId;
+        this.url = url;
+        this.position = position;
+    }
+
+    public static Image of(ImageType type, Long relatedId, String url, int position) {
+        return Image.builder()
+                .imageType(type)
+                .relatedId(relatedId)
+                .url(url)
+                .position(position)
+                .build();
+    }
+
+    public void changePosition(int position) {
+        this.position = position;
+    }
 }
