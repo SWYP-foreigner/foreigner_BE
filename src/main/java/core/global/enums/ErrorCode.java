2b757e2d9278c6e566d0faa991e14f518f8698b6
usingjun <usingjun99@gmail.com>
2025-08-18 15:20:45 +0900

    feat: ErrorCode 추가

--- src/main/java/core/global/enums/ErrorCode.java
+++ src/main/java/core/global/enums/ErrorCode.java
@@ -12,9 +12,8 @@ public enum ErrorCode {
     POST_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 게시물입니다."),
     POST_EDIT_FORBIDDEN(HttpStatus.FORBIDDEN, "해당 글을 수정할 권한이 없습니다."),
     POST_DELETE_FORBIDDEN(HttpStatus.FORBIDDEN, "해당 글을 삭제할 권한이 없습니다."),
-    BOARD_AND_POST_NOT_MATCHED(HttpStatus.NOT_FOUND, "게시판과 게시물이 매치되지 않습니다."),
     NOT_AVAILABLE_ANONYMOUS(HttpStatus.BAD_REQUEST, "익명이 허용되지 않는 카테고리입니다."),
-    INVALID_BOARD_CATEGORY(HttpStatus.BAD_REQUEST, "유효하지 않은 category입니다."),
+    INVALID_BOARD_CATEGORY(HttpStatus.BAD_REQUEST, "유효하지 않은 카테고리입니다."),
 
     COMMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 댓글입니다."),
     INVALID_PARENT_COMMENT(HttpStatus.NOT_FOUND, "대댓글하려는 댓글이 적합하지 않습니다."),
@@ -31,8 +30,12 @@ public enum ErrorCode {
     OAUTH_PROVIDER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "OAuth 공급자에서 에러가 발생했습니다."),
     MISSING_SOCIAL_INFO(HttpStatus.BAD_REQUEST, "소셜 로그인 필수 정보가 누락되었습니다."),
     USER_UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "로그인이 필요합니다."),
-    INVALID_UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "로그인 되어있지 않습니다.");
+    INVALID_UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "로그인 되어있지 않습니다."),
 
+    LIKE_NOT_FOUND(HttpStatus.BAD_REQUEST, "좋아요가 존재하지 않습니다."),
+    LIKE_ALREADY_EXIST(HttpStatus.BAD_REQUEST, "좋아요가 이미 존재합니다."),
+    BOOKMARK_ALREADY_EXIST(HttpStatus.BAD_REQUEST, "북마크가 이미 존재합니다."),
+    BOOKMARK_NOT_FOUND(HttpStatus.NOT_FOUND, "북마크가 존재하지 않습니다.");
 
 
     private final HttpStatus errorCode;
