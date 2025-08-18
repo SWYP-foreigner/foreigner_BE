3dbd87d638756e8c5d9cde6b35b7ab2dbccc018a
usingjun <usingjun99@gmail.com>
2025-08-18 15:18:17 +0900

    bugfix: 매핑 버그 수정

--- src/main/java/core/global/like/repository/LikeRepository.java
+++ src/main/java/core/global/like/repository/LikeRepository.java
@@ -3,6 +3,7 @@ package core.global.like.repository;
 import core.global.enums.LikeType;
 import core.global.like.entity.Like;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -11,19 +12,21 @@ import java.util.Optional;
 
 public interface LikeRepository extends JpaRepository<Like, Long> {
 
-    @Query("select l from Like l where l.relatedId= :postId and l.type= :likeType and l.user.name= :username")
-    Optional<Like> findLikeByUsernameAndType(@Param("username") String username, @Param("postId") Long postId, @Param("likeType") LikeType likeType);
+    long countByTypeAndRelatedId(LikeType type, Long relatedId);
+
+    @Query("select l from Like l where l.relatedId= :id and l.type= :likeType and l.user.name= :name")
+    Optional<Like> findLikeByUsernameAndType(@Param("name") String name, @Param("id") Long id, @Param("likeType") LikeType likeType);
 
     @Query("""
     select l.relatedId, count(l.id)
     from Like l
     where l.type = :type
-      and l.relatedId in :commentIds
+      and l.relatedId in :ids
     group by l.relatedId
     """)
     List<Object[]> countByRelatedIds(
             @Param("type") LikeType type,
-            @Param("commentIds") List<Long> commentIds
+            @Param("ids") List<Long> ids
     );
 
     @Query("""
@@ -31,11 +34,21 @@ public interface LikeRepository extends JpaRepository<Like, Long> {
     from Like l
     where l.type = :type
       and l.user.id = :userId
-      and l.relatedId in :commentIds
+      and l.relatedId in :ids
     """)
     List<Long> findRelatedIdsLikedByUser(
             @Param("type") LikeType type,
             @Param("userId") Long userId,
-            @Param("commentIds") List<Long> commentIds
+            @Param("ids") List<Long> ids
     );
+
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
+    @Query("""
+    select l.relatedId
+    from Like l
+    where l.type = :type
+      and l.user.name = :name
+      and l.relatedId in :id
+    """)
+    Long deleteByUserNameAndIdAndType(String name, Long id, LikeType likeType);
 }
