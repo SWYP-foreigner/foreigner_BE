e263359366a4a71962b474eb8daa6822ecc61ff7
usingjun <usingjun99@gmail.com>
2025-08-19 12:00:28 +0900

    feat: ErrorCode 수정

--- src/main/java/core/global/enums/ErrorCode.java
+++ src/main/java/core/global/enums/ErrorCode.java
@@ -24,17 +24,10 @@ public enum ErrorCode {
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 유저입니다."),
 
     PLACE_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 장소입니다."),
-    INVALID_PLACE_CATEGORY(HttpStatus.BAD_REQUEST, "유효하지 않은 category입니다."),
 
-    TRIP_PLAN_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 여행일정입니다."),
-    TRIP_DAY_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 여행일입니다."),
-    TOURIST_SPOT_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 여행지입니다."),
-    INVALID_TRIP_ORDER_INDEX(HttpStatus.BAD_REQUEST, "이동할 수 없는 Index입니다."),
-
-    FOLLOW_ALREADY_EXISTS(HttpStatus.BAD_REQUEST,"이미 팔로우 한 대상입니다."),
-    USERSPOT_IS_ALREADY_CREATED(HttpStatus.BAD_REQUEST, "이미 존재하는 나만의 장소입니다."),
+    FOLLOW_ALREADY_EXISTS(HttpStatus.BAD_REQUEST, "이미 팔로우 한 대상입니다."),
     INVALID_TRIP_STATUS(HttpStatus.BAD_REQUEST, "유효하지 않은 status입니다."),
-    FOLLOWER_NOT_FOUND(HttpStatus.NOT_FOUND,"팔로워를 찾을 수 없습니다."),
+    FOLLOWER_NOT_FOUND(HttpStatus.NOT_FOUND, "팔로워를 찾을 수 없습니다."),
 
 
     UPDATE_NOT_PROCESSED(HttpStatus.BAD_REQUEST, "프로필 수정에 업데이트에 실패했습니다."),
@@ -54,12 +47,10 @@ public enum ErrorCode {
     LIKE_NOT_FOUND(HttpStatus.BAD_REQUEST, "좋아요가 존재하지 않습니다."),
     LIKE_ALREADY_EXIST(HttpStatus.BAD_REQUEST, "좋아요가 이미 존재합니다."),
     BOOKMARK_ALREADY_EXIST(HttpStatus.BAD_REQUEST, "북마크가 이미 존재합니다."),
-    BOOKMARK_NOT_FOUND(HttpStatus.NOT_FOUND, "북마크가 존재하지 않습니다."),
-    CITY_NOT_FOUND(HttpStatus.NOT_FOUND, "요청하신 도시를 찾을 수 없습니다."),
-    INVALID_UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "로그인 되어있지 않습니다.");
+    BOOKMARK_NOT_FOUND(HttpStatus.NOT_FOUND, "북마크가 존재하지 않습니다.");
 
 
-    private final HttpStatus  errorCode;
+    private final HttpStatus errorCode;
     private final String message;
 
 }
