2e7a743aafb342a0c33792bc8b7218ae096fd244
lixxce5017 <lixxce5017@gmail.com>
2025-08-27 03:47:24 +0900

    feat: 시큐리티 필터 요청 변경

--- src/main/java/core/global/config/JwtTokenFilter.java
+++ src/main/java/core/global/config/JwtTokenFilter.java
@@ -43,6 +43,21 @@ public class JwtTokenFilter extends OncePerRequestFilter {
 
     private final AntPathMatcher pathMatcher = new AntPathMatcher();
 
+    private static final List<String> EXCLUDE_URLS = List.of(
+            "/api/v1/member/google/app-login",
+            "/api/v1/member/google/**",
+            "/api/v1/member/apple/**",
+            "/swagger-ui/**",
+            "/v3/api-docs/**",
+            "/swagger-resources/**",
+            "/swagger-ui.html"
+    );
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) {
+        String requestUri = request.getRequestURI();
+        return EXCLUDE_URLS.stream().anyMatch(url -> pathMatcher.match(url, requestUri));
+    }
 
     @PostConstruct
     public void init() {
