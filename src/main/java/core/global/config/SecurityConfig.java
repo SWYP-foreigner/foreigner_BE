d827dda3a8e916f9fc95c6044ba8abd47a631653
lixxce5017 <lixxce5017@gmail.com>
2025-08-27 03:09:07 +0900

    feat: 스프링시큐리티 jwt 예외처리 커스텀 추가

--- src/main/java/core/global/config/SecurityConfig.java
+++ src/main/java/core/global/config/SecurityConfig.java
@@ -2,12 +2,18 @@ package core.global.config;
 
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import core.global.enums.ErrorCode;
+import core.global.exception.BusinessException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
@@ -18,6 +24,7 @@ import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
+@Slf4j
 public class SecurityConfig {
 
     private final JwtTokenFilter jwtTokenFilter;
@@ -64,4 +71,34 @@ public class SecurityConfig {
         src.registerCorsConfiguration("/**", c);
         return src;
     }
+
+    @Bean
+    public AuthenticationEntryPoint unauthorizedHandler() {
+        return (request, response, authException) -> {
+            log.error("필터 체인 예외 발생", authException);
+
+
+            if (authException.getCause() instanceof BusinessException) {
+                BusinessException be = (BusinessException) authException.getCause();
+                ErrorCode errorCode = be.getErrorCode();
+
+                response.setStatus(errorCode.getErrorCode().value());
+                response.setContentType("application/json;charset=UTF-8");
+
+                ObjectMapper objectMapper = new ObjectMapper();
+                String errorResponse = objectMapper.writeValueAsString(
+                        new ErrorResponseDto(errorCode.getErrorCode().value(), errorCode.getMessage())
+                );
+                response.getWriter().write(errorResponse);
+            } else {
+                response.setStatus(HttpStatus.UNAUTHORIZED.value());
+                response.setContentType("application/json;charset=UTF-8");
+                response.getWriter().write(
+                        "{\"error\":\"unauthorized\", \"message\":\"" + authException.getMessage() + "\"}"
+                );
+            }
+        };
+    }
+
+    public record ErrorResponseDto(int code, String message) {}
 }
