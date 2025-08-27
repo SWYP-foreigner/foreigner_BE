beb241bbe86607567b161a9d3a5fc972a536d404
lixxce5017 <lixxce5017@gmail.com>
2025-08-27 13:02:04 +0900

    chore: jwt 익사이파이어 타임 수정

--- src/main/java/core/global/config/SecurityConfig.java
+++ src/main/java/core/global/config/SecurityConfig.java
@@ -52,7 +52,8 @@ public class SecurityConfig {
                                 "/swagger-ui/**",
                                 "/v3/api-docs/**",
                                 "/swagger-resources/**",
-                                "/swagger-ui.html"
+                                "/swagger-ui.html",
+                                "/error"
                         ).permitAll()
 
                         .anyRequest().authenticated()
