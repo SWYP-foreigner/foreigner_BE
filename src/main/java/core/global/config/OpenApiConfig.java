7dffe3508a627ceb470c508a4d92750bcaf88a5f
Jeon Hanjoon <hjun230604@gmail.com>
2025-08-31 00:05:51 +0900

    Open Config 수정

--- src/main/java/core/global/config/OpenApiConfig.java
+++ src/main/java/core/global/config/OpenApiConfig.java
@@ -2,13 +2,8 @@ package core.global.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.servers.Server;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.Components;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,13 +11,8 @@ import org.springframework.context.annotation.Configuration;
 import java.util.List;
 
 @Configuration
-@OpenAPIDefinition(info = @Info(title = "API 문서", version = "v1"))
-@SecurityScheme(
-        name = "bearerAuth",            // 스키마 이름
-        type = SecuritySchemeType.HTTP, // HTTP 인증
-        scheme = "bearer",              // Bearer 방식
-        bearerFormat = "JWT",           // UI 힌트
-        in = SecuritySchemeIn.HEADER    // Authorization 헤더 사용
+@OpenAPIDefinition(
+        info = @Info(title = "API 문서", version = "v1")
 )
 public class OpenApiConfig {
 
@@ -31,20 +21,9 @@ public class OpenApiConfig {
 
     @Bean
     public OpenAPI openAPI() {
-        Server httpsServer = new Server().url(serverUrl);
-
-        // 전역 Security 요구사항 추가 (모든 API에 기본 적용)
-        SecurityRequirement securityItem = new SecurityRequirement().addList("bearerAuth");
-
+        Server httpsServer = new Server();
+        httpsServer.setUrl(serverUrl);
         return new OpenAPI()
-                .servers(List.of(httpsServer))
-                .components(new Components().addSecuritySchemes(
-                        "bearerAuth",
-                        new io.swagger.v3.oas.models.security.SecurityScheme()
-                                .type(io.swagger.v3.oas.models.security.SecurityScheme.Type.HTTP)
-                                .scheme("bearer")
-                                .bearerFormat("JWT")
-                ))
-                .addSecurityItem(securityItem);
+                .servers(List.of(httpsServer));
     }
 }
