7fd35c900ac0bddf181a81afb5a3b276f3b9367e
lixxce5017 <lixxce5017@gmail.com>
2025-09-05 00:50:21 +0900

    chore: createRoom 로그 추가

--- src/main/java/core/domain/chat/controller/ChatController.java
+++ src/main/java/core/domain/chat/controller/ChatController.java
@@ -26,7 +26,6 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -36,14 +35,14 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-
+import lombok.extern.slf4j.Slf4j;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Tag(name = "채팅 API", description = "1:1 채팅, 그룹 채팅, 메시지 검색/삭제 등 채팅 기능 API")
 @RestController
 @RequestMapping("/api/v1/chat")
-@Slf4j
+
 public class ChatController {
     private final ChatService chatService;
     private final Logger log = LoggerFactory.getLogger(ChatController.class);
