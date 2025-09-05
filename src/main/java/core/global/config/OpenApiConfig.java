169295437af88abb7f3c9baab69c5a4684ee2343
Jeon Hanjoon <hjun230604@gmail.com>
2025-09-05 22:37:41 +0900

    수정 5

--- src/main/java/core/global/config/OpenApiConfig.java
+++ src/main/java/core/global/config/OpenApiConfig.java
@@ -9,18 +9,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.List;
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
-import io.swagger.v3.oas.models.servers.Server;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import java.util.List;
 
 @Configuration
 @OpenAPIDefinition(
@@ -35,21 +23,7 @@ public class OpenApiConfig {
     public OpenAPI openAPI() {
         Server httpsServer = new Server();
         httpsServer.setUrl(serverUrl);
-
-        // JWT 토큰 인증을 위한 SecurityScheme 정의
-        SecurityScheme securityScheme = new SecurityScheme()
-                .type(SecurityScheme.Type.HTTP)
-                .scheme("bearer")
-                .bearerFormat("JWT")
-                .in(SecurityScheme.In.HEADER)
-                .name("Authorization");
-
-        // 전역 보안 요구사항 설정
-        SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearerAuth");
-
         return new OpenAPI()
-                .servers(List.of(httpsServer))
-                .components(new Components().addSecuritySchemes("bearerAuth", securityScheme))
-                .security(List.of(securityRequirement));
+                .servers(List.of(httpsServer));
     }
 }
