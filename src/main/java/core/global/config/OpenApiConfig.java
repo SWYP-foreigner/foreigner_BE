9a6f39c82cb482bcd86ddc5c1f0521ee8ec8d3bc
Jeon Hanjoon <hjun230604@gmail.com>
2025-09-07 06:05:42 +0900

    팔로우 로직 수정(기존 코드 원상복귀+yml 수정)

--- src/main/java/core/global/config/OpenApiConfig.java
+++ src/main/java/core/global/config/OpenApiConfig.java
@@ -2,10 +2,7 @@ package core.global.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -24,17 +21,9 @@ public class OpenApiConfig {
 
     @Bean
     public OpenAPI openAPI() {
-        // Bearer 인증을 위한 SecurityScheme 설정
-        SecurityScheme securityScheme = new SecurityScheme()
-                .type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
-                .in(SecurityScheme.In.HEADER).name("Authorization");
-        SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearerAuth");
-
         Server httpsServer = new Server();
         httpsServer.setUrl(serverUrl);
         return new OpenAPI()
-                .components(new Components().addSecuritySchemes("bearerAuth", securityScheme))
-                .security(List.of(securityRequirement))
                 .servers(List.of(httpsServer));
     }
 }
