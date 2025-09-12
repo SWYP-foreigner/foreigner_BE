038ee81919af629d287916a14e2f088a9cf27efe
lixxce5017 <lixxce5017@gmail.com>
2025-09-12 20:15:06 +0900

    chore: ApplePublicKey 파라매터 use 추가

--- src/main/java/core/global/dto/ApplePublicKey.java
+++ src/main/java/core/global/dto/ApplePublicKey.java
@@ -7,5 +7,5 @@ import lombok.Setter;
 
 import java.util.List;
 
-public record ApplePublicKey(String kty, String kid, String alg, String n, String e) {
+public record ApplePublicKey(String kty, String kid,String use, String alg, String n, String e) {
 }
