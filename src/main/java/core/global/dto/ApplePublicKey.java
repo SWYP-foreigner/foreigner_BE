c6faeb56c218c829910582c9cfec6c30799cfcfc
lixxce5017 <lixxce5017@gmail.com>
2025-09-14 17:09:26 +0900

    chore: ApplePublicKey json 매칭되게 변경

--- src/main/java/core/global/dto/ApplePublicKey.java
+++ src/main/java/core/global/dto/ApplePublicKey.java
@@ -1,18 +1,12 @@
 package core.global.dto;
 
-import core.global.enums.ErrorCode;
-import core.global.exception.BusinessException;
-import lombok.Getter;
-import lombok.Setter;
-
-import java.util.List;
 
 public record ApplePublicKey(
-        String kty, // Key Type (EC)
-        String kid, // Key ID
-        String use, // Usage (sig)
-        String alg, // Algorithm (ES256)
-        String crv, // Curve (P-256)
-        String x,   // X Coordinate
-        String y    // Y Coordinate
+        String kty,
+        String kid,
+        String use,
+        String alg,
+        String crv,
+        String x,
+        String y
 ) {}
