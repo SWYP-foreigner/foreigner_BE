19d21574223f6137121fbead4249ff3224eabd40
lixxce5017 <lixxce5017@gmail.com>
2025-09-15 13:16:43 +0900

    chore: FeignClient RequestParam로 변경

--- src/main/java/core/global/service/AppleClient.java
+++ src/main/java/core/global/service/AppleClient.java
@@ -5,11 +5,14 @@ import core.global.dto.ApplePublicKeyResponse;
 import core.global.dto.AppleRefreshTokenResponse;
 import core.global.dto.AppleTokenResponse;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.MediaType;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Map;
 
 @FeignClient(name = "appleClient", url = "https://appleid.apple.com")
 public interface AppleClient {
@@ -19,19 +22,18 @@ public interface AppleClient {
      */
     @GetMapping("/auth/keys")
     ApplePublicKeyResponse getApplePublicKeys();
-    /**
-     * 발급 받은 엑세스나 리프레쉬 토큰을 무효화 시킨다.
-     * @param form
-     */
-    @PostMapping(value = "/auth/revoke", consumes = "application/x-www-form-urlencoded")
-    void revoke(MultiValueMap<String, String> form);
 
     /**
-     * 토큰을 요청
-     * 정확히는 (access+refresh+id) token 요청하고 받음
-     * @param form
-     * @return
+     * 발급 받은 엑세스나 리프레쉬 토큰을 무효화 시킨다.
+     * application/x-www-form-urlencoded 로 보내야 함
      */
-    @PostMapping(value = "/auth/token", consumes = "application/x-www-form-urlencoded")
-    AppleRefreshTokenResponse getToken(MultiValueMap<String, String> form);
+    @PostMapping(value = "/auth/revoke", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    void revoke(@RequestParam Map<String, ?> form);
+
+    /**
+     * 토큰 요청 (access+refresh+id 토큰 받음)
+     * application/x-www-form-urlencoded 로 보내야 함
+     */
+    @PostMapping(value = "/auth/token", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    AppleRefreshTokenResponse getToken(@RequestParam Map<String, ?> form);
 }
