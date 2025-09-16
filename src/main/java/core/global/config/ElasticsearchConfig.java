31ca51b00d54cddfc8958f8e3c3768c2cf577b11
usingjun <usingjun99@gmail.com>
2025-09-16 20:42:05 +0900

    bugfix: es 연동

--- src/main/java/core/global/config/ElasticsearchConfig.java
+++ src/main/java/core/global/config/ElasticsearchConfig.java
@@ -5,6 +5,10 @@ import co.elastic.clients.json.jackson.JacksonJsonpMapper;
 import co.elastic.clients.transport.rest_client.RestClientTransport;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
 import org.elasticsearch.client.RestClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -14,8 +18,22 @@ import org.springframework.context.annotation.Configuration;
 public class ElasticsearchConfig {
 
     @Bean
-    public RestClient restClient(@Value("${spring.elasticsearch.uris}") String esUrl) {
-        return RestClient.builder(HttpHost.create(esUrl)).build();
+    public RestClient restClient(
+            @Value("${spring.elasticsearch.uris}") String esUrl,
+            @Value("${spring.elasticsearch.username:}") String esUsername,
+            @Value("${spring.elasticsearch.password:}") String esPassword
+    ) {
+        // Basic 인증 설정
+        BasicCredentialsProvider creds = new BasicCredentialsProvider();
+        if (!esUsername.isEmpty()) {
+            creds.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(esUsername, esPassword));
+        }
+
+        return RestClient.builder(org.apache.http.HttpHost.create(esUrl))
+                .setHttpClientConfigCallback((HttpAsyncClientBuilder http) ->
+                        http.setDefaultCredentialsProvider(creds)
+                )
+                .build();
     }
 
     @Bean
