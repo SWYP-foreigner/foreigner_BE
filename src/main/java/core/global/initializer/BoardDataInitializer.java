17391011a02b550ff0658b36d7923da556a80846
usingjun <usingjun99@gmail.com>
2025-09-18 12:34:07 +0900

    chore: main에도 카테고리 자동 생성

--- src/main/java/core/global/initializer/BoardDataInitializer.java
+++ src/main/java/core/global/initializer/BoardDataInitializer.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
-@Profile({"dev","local", "main"})
+@Profile({"dev","local", "prod"})
 @RequiredArgsConstructor
 public class BoardDataInitializer implements CommandLineRunner {
 
