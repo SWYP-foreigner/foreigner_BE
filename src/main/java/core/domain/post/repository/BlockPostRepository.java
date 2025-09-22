eea161b2c35f273fdc2e0d87781d7fcedb199150
usingjun <usingjun99@gmail.com>
2025-09-22 22:34:30 +0900

    feat: 게시글 차단으로 게시글 숨기기

--- src/main/java/core/domain/post/repository/BlockPostRepository.java
+++ src/main/java/core/domain/post/repository/BlockPostRepository.java
@@ -1,11 +1,12 @@
 package core.domain.post.repository;
 
 import core.domain.post.entity.BlockPost;
+import feign.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface BlockPostRepository extends JpaRepository<BlockPost, Long> {
 
     @Query("select count(b)>0 from BlockPost b where b.user.id=:userId and b.post.id=:postId")
-    boolean existsBlock(Long userId, Long postId);
+    boolean existsBlock(@Param("userId") Long userId, @Param("postId") Long postId);
 }
