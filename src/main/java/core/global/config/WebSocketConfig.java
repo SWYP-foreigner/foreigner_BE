04b6e4b87402ae5fa2daf5e75af1e8ff8fe4bf2a
lixxce5017 <lixxce5017@gmail.com>
2025-11-07 06:47:04 +0900

    chore: 주석 삭제

--- src/main/java/core/global/config/WebSocketConfig.java
+++ src/main/java/core/global/config/WebSocketConfig.java
@@ -1,22 +1,21 @@
 package core.global.config;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired; // 👈 Import
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.handler.invocation.HandlerMethodArgumentResolver;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.messaging.simp.annotation.support.SimpAnnotationMethodMessageHandler; // 👈 Import
+import org.springframework.messaging.simp.annotation.support.SimpAnnotationMethodMessageHandler;
 import org.springframework.security.messaging.context.AuthenticationPrincipalArgumentResolver;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
 import org.springframework.context.annotation.Bean;
-import jakarta.annotation.PostConstruct; // 👈 Import (Spring Boot 3)
-// import javax.annotation.PostConstruct; // 👈 (Spring Boot 2)
-import java.util.ArrayList; // 👈 Import
-import java.util.List; // 👈 Import
+import jakarta.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -37,11 +36,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         this.stompChannelInterceptor = stompChannelInterceptor;
     }
 
-    /**
-     * ⭐️ [변경 2: 핵심]
-     * @PostConstruct를 이용해, Spring이 Bean을 모두 만든 후
-     * SimpAnnotationMethodMessageHandler에 수동으로 Resolver를 등록합니다.
-     */
     @PostConstruct
     public void addArgumentResolver() {
         log.info("Manually adding AuthenticationPrincipalArgumentResolver to SimpAnnotationMethodMessageHandler...");
@@ -50,7 +44,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
                 simpAnnotationMethodMessageHandler.getArgumentResolvers();
 
         List<HandlerMethodArgumentResolver> newResolvers = new ArrayList<>();
-        // ⭐️ @Bean으로 만든 메소드를 호출하여 주입
         newResolvers.add(authenticationPrincipalArgumentResolver());
         newResolvers.addAll(existingResolvers);
 
