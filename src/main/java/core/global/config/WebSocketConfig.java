bc7ce1a76092b8dd49b0df3b1fb4b8fbcee9e3da
lixxce5017 <lixxce5017@gmail.com>
2025-11-07 07:35:41 +0900

    fix(websocket):WebSocketConfig 순환참조 문제해결

--- src/main/java/core/global/config/WebSocketConfig.java
+++ src/main/java/core/global/config/WebSocketConfig.java
@@ -8,7 +8,7 @@ import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.messaging.simp.annotation.support.SimpAnnotationMethodMessageHandler;
 import org.springframework.security.messaging.context.AuthenticationPrincipalArgumentResolver;
-import org.springframework.security.messaging.context.SecurityContextChannelInterceptor; // 👈 1. Import 추가
+import org.springframework.security.messaging.context.SecurityContextChannelInterceptor;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -24,17 +24,11 @@ import java.util.List;
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     private final StompChannelInterceptor stompChannelInterceptor;
-
-    @Autowired
     private SimpAnnotationMethodMessageHandler simpAnnotationMethodMessageHandler;
-
     @Bean
     public AuthenticationPrincipalArgumentResolver authenticationPrincipalArgumentResolver() {
         return new AuthenticationPrincipalArgumentResolver();
     }
-
-    // ⭐️ [추가 1]
-    // SecurityContextHolder를 채워줄 인터셉터를 Bean으로 등록합니다.
     @Bean
     public SecurityContextChannelInterceptor securityContextChannelInterceptor() {
         return new SecurityContextChannelInterceptor();
@@ -43,6 +37,12 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public WebSocketConfig(StompChannelInterceptor stompChannelInterceptor) {
         this.stompChannelInterceptor = stompChannelInterceptor;
     }
+
+    @Autowired
+    public void setSimpAnnotationMethodMessageHandler(SimpAnnotationMethodMessageHandler simpAnnotationMethodMessageHandler) {
+        this.simpAnnotationMethodMessageHandler = simpAnnotationMethodMessageHandler;
+    }
+
 
     @PostConstruct
     public void addArgumentResolver() {
@@ -56,13 +56,9 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         log.info("AuthenticationPrincipalArgumentResolver added manually.");
     }
 
-    // ⭐️ [추가 2]
-    // 채널에 두 개의 인터셉터를 모두 등록합니다.
     @Override
     public void configureClientInboundChannel(ChannelRegistration registration) {
-        // 1. (CONNECT 시 실행) JWT 인증을 수행하고 세션에 userAuth를 저장합니다.
         registration.interceptors(stompChannelInterceptor);
-        // 2. (모든 메시지 실행) 세션의 userAuth를 SecurityContextHolder로 복사합니다.
         registration.interceptors(securityContextChannelInterceptor());
     }
 
