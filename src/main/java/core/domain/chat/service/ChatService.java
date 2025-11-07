f1d629aeb330d1054c2fabf39f2c7e129c582bf2
usingjun <usingjun99@gmail.com>
2025-11-08 00:42:22 +0900

    bugfix: 채팅 로그 찍기

--- src/main/java/core/domain/chat/service/ChatService.java
+++ src/main/java/core/domain/chat/service/ChatService.java
@@ -473,8 +473,11 @@ public class ChatService {
 
     @Transactional(readOnly = true)
     public List<ChatMessageResponse> searchMessages(Long roomId, Long userId, String keyword) {
+        log.info("roomId " + roomId);
+        log.info("userId " + userId);
         String email = SecurityContextHolder.getContext().getAuthentication().getName();
 
+        log.info("email " + email);
         User user = userRepository.findByEmail(email)
                 .orElseThrow(() -> new BusinessException(ErrorCode.USER_NOT_FOUND));
 
