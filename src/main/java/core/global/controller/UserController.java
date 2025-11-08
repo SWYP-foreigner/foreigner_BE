d97622ab59ef3c3cc2852e6b9d9f430ef5dcf0c9
usingjun <usingjun99@gmail.com>
2025-11-08 13:44:41 +0900

    bugfix: 유저 profile 수정 및 셋업 valid 로직 추가

--- src/main/java/core/global/controller/UserController.java
+++ src/main/java/core/global/controller/UserController.java
@@ -211,7 +211,7 @@ public class UserController {
 
     @PatchMapping("/profile/setup")
     @Operation(summary = "처음 회원가입시 프로필 이미지랑 함께 자기소개 작성 ", description = "현재 사용자의 프로필 정보를 세팅합니다.")
-    public ResponseEntity<Void> updateProfile(@RequestBody UserSetupRequest dto) {
+    public ResponseEntity<Void> updateProfile(@Valid @RequestBody UserSetupRequest dto) {
         userService.setupUserProfile(dto);
         return ResponseEntity.ok(null);
     }
