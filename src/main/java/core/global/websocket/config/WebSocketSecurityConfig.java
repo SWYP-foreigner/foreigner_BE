69d7e26561001b2f816e9e97a5cd4729961576b7
lixxce5017 <lixxce5017@gmail.com>
2025-11-11 12:59:10 +0900

    refactor(websocket): DIWebSocketSecurityConfig SCONNECT 패킷 허용

--- src/main/java/core/global/websocket/config/WebSocketSecurityConfig.java
+++ src/main/java/core/global/websocket/config/WebSocketSecurityConfig.java
@@ -1,6 +1,8 @@
 package core.global.websocket.config;
 
 import org.springframework.context.annotation.Configuration;
+// 1. SimpMessageType 임포트
+import org.springframework.messaging.simp.SimpMessageType;
 import org.springframework.security.config.annotation.web.messaging.MessageSecurityMetadataSourceRegistry;
 import org.springframework.security.config.annotation.web.socket.AbstractSecurityWebSocketMessageBrokerConfigurer;
 import org.springframework.security.config.annotation.web.socket.EnableWebSocketSecurity;
@@ -9,15 +11,10 @@ import org.springframework.security.config.annotation.web.socket.EnableWebSocket
 @EnableWebSocketSecurity
 public class WebSocketSecurityConfig extends AbstractSecurityWebSocketMessageBrokerConfigurer {
 
-    /**
-     * 이 클래스가 존재하는 것만으로도
-     * SecurityContextChannelInterceptor와 AuthenticationPrincipalArgumentResolver가
-     * 보안 기능이 활성화된 버전으로 빈 컨테이너에 등록됩니다.
-     */
-
     @Override
     protected void configureInbound(MessageSecurityMetadataSourceRegistry messages) {
         messages
+                .simpTypeMatchers(SimpMessageType.DISCONNECT).permitAll()
                 .simpDestMatchers("/app/**").authenticated()
                 .anyMessage().authenticated();
     }
