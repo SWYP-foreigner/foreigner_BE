2b70cf09bf5567ccb2fdeebf7511bfd6b7023b38
lixxce5017 <lixxce5017@gmail.com>
2025-11-17 13:33:11 +0900

    feat: ApiResponse fail 롤백

--- src/main/java/core/global/dto/ApiResponse.java
+++ src/main/java/core/global/dto/ApiResponse.java
@@ -4,22 +4,19 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 
 import java.time.LocalDateTime;
 
-public record ApiResponse<T>(
-        String message,
-        T data,
-        String errorCode,
-        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
-        LocalDateTime timestamp
-) {
+public record ApiResponse<T>(String message , T data,
+                             @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
+                             LocalDateTime timestamp) {
     public static <T> ApiResponse<T> success(T data) {
-        return new ApiResponse<>("success", data, null, LocalDateTime.now());
+        return new ApiResponse<>("success", data, LocalDateTime.now());
     }
 
-    public static <T> ApiResponse<T> error(String message, String errorCode) {
-        return new ApiResponse<>(message, null, errorCode, LocalDateTime.now());
+
+    public static <T> ApiResponse<T> error(String message) {
+        return new ApiResponse<>(message, null, LocalDateTime.now());
+    }
+    public static <T> ApiResponse<T> fail(String message) {
+        return new ApiResponse<>(message, null, LocalDateTime.now());
     }
 
-    public static <T> ApiResponse<T> fail(String message, String errorCode) {
-        return new ApiResponse<>(message, null, errorCode, LocalDateTime.now());
-    }
 }
