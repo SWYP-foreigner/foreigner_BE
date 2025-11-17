c50abe109f7d7d70598719453695c6bbe7d699bc
lixxce5017 <lixxce5017@gmail.com>
2025-11-17 20:30:30 +0900

    feat: 토큰 삭제 로직 추가

--- src/main/java/core/domain/notification/repository/NotificationRepository.java
+++ src/main/java/core/domain/notification/repository/NotificationRepository.java
@@ -44,4 +44,6 @@ public interface NotificationRepository extends JpaRepository<Notification, Long
     void deleteAllByActorId(Long actorId);
 
     Slice<Notification> findByCreatedAtBefore(Instant cutoffDate, Pageable pageable);
+    void deleteByUserAndDeviceToken(User user, String deviceToken);
+
 }
