30b191fc29651ba42b1fe6131c8958ebd75193c2
lixxce5017 <lixxce5017@gmail.com>
2025-11-23 22:29:32 +0900

    chore:GroupChatSearchResponse id 변수명 원복

--- src/main/java/core/domain/chat/service/ChatEventListener.java
+++ src/main/java/core/domain/chat/service/ChatEventListener.java
@@ -47,7 +47,7 @@ public class ChatEventListener {
             if (commonSummary != null) {
                 int personalUnreadCount = calculateUnreadCount(message.roomId(), recipientId);
                 ChatRoomSummaryResponse personalSummary = new ChatRoomSummaryResponse(
-                        commonSummary.chatRoomId(),
+                        commonSummary.roomId(),
                         commonSummary.roomName(),
                         commonSummary.lastMessageContent(),
                         commonSummary.lastMessageTime(),
