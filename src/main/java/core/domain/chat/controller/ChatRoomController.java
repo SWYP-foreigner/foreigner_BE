f8fc68cf8344e0707fc0e22688ae8c3812533191
lixxce5017 <lixxce5017@gmail.com>
2025-11-23 15:27:43 +0900

    chore(chat): 그룹채팅 api 스웨거 수정, dto 이름 변경

--- src/main/java/core/domain/chat/controller/ChatRoomController.java
+++ src/main/java/core/domain/chat/controller/ChatRoomController.java
@@ -158,7 +158,7 @@ public class ChatRoomController {
     @Operation(summary = "인기 그룹 채팅방 10개 조회", description = "참여자가 가장 많은 그룹 채팅방 10개를 조회합니다.")
     @ApiResponses({
             @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "성공",
-                    content = @Content(schema = @Schema(implementation = GroupChatSearchResponse.class))
+                    content = @Content(schema = @Schema(implementation = GroupChatMainResponse.class))
             )
     })
     @GetMapping("/group/popular")
