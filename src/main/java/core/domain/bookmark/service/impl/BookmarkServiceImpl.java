d22245f37228993df1805ad4abf598fd3a78e7db
usingjun <usingjun99@gmail.com>
2025-11-29 15:08:10 +0900

    refactor: isBookmarked로 변경, authorId 추가

--- src/main/java/core/domain/bookmark/service/impl/BookmarkServiceImpl.java
+++ src/main/java/core/domain/bookmark/service/impl/BookmarkServiceImpl.java
@@ -136,6 +136,10 @@ public class BookmarkServiceImpl implements BookmarkService {
                 ? "Anonymity"
                 : (p.getAuthor() != null ? p.getAuthor().getFirstName() +" "+ p.getAuthor().getLastName() : null);
 
+        Long authorId = Boolean.TRUE.equals(p.getAnonymous())
+                ? null
+                : (p.getAuthor() != null ? p.getAuthor().getId() : null);
+
         Long postId = p.getId();
         Instant time = p.getCreatedAt();
         Boolean anonymous = p.getAnonymous();
@@ -153,6 +157,7 @@ public class BookmarkServiceImpl implements BookmarkService {
         return new BookmarkItem(
                 b.getId(),
                 postId,
+                authorId,
                 authorName,
                 safeTrim(p.getContent()),
                 time,
