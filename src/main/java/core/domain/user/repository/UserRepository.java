0dbb38fcefe800b7ab6766ab53d4333c45f63cc4
lixxce5017 <lixxce5017@gmail.com>
2025-12-06 18:31:05 +0900

    chore: findActiveUserTokens 삭제

--- src/main/java/core/domain/user/repository/UserRepository.java
+++ src/main/java/core/domain/user/repository/UserRepository.java
@@ -189,16 +189,4 @@ public interface UserRepository extends JpaRepository<User, Long>, UserRepositor
     Object[] visitorShareOverall();
 
 
-    /*
-            * 1. User 엔티티 전체를 가져오지 않고 'fcmToken'만 가져옵니다. (메모리 절약)
-            * 2. DB 인덱스를 활용해 '국가'와 '최근 접속일'로 필터링합니다. (속도 향상)
-            */
-    @Query("SELECT u.fcmToken FROM User u " +
-            "WHERE u.country = :targetCountry " +
-            "AND u.lastSeenAt >= :activeSince")
-    Slice<String> findActiveUserTokens(
-            @Param("targetCountry") String targetCountry,
-            @Param("activeSince") Instant activeSince,
-            Pageable pageable
-    );
 }
