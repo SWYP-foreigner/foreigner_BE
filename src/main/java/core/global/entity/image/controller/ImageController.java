6bb460f7c225ab089911c08f72ae07c89e149eca
usingjun <usingjun99@gmail.com>
2025-12-10 09:53:46 +0900

    chore: 에러코드 개선 작업

--- src/main/java/core/global/entity/image/controller/ImageController.java
+++ src/main/java/core/global/entity/image/controller/ImageController.java
@@ -1,14 +1,14 @@
 package core.global.entity.image.controller;
 
+import core.global.docs.annotations.ImageErrorCodeDocs;
 import core.global.dto.ApiResponse;
-import core.global.dto.UpsertChatRoomImageRequest;
 import core.global.entity.image.dto.ImageDto;
 import core.global.entity.image.dto.PresignedUrlRequest;
 import core.global.entity.image.dto.PresignedUrlResponse;
 import core.global.entity.image.service.ImageService;
+import core.global.enums.errorcode.ImageErrorCode;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -28,6 +28,7 @@ public class ImageController {
     @Operation(summary = "다건 Presigned URL 발급",
             description = "uploadSessionId + files[] 기반으로 Presigned URL을 일괄 발급")
     @PostMapping("/presign")
+    @ImageErrorCodeDocs({ImageErrorCode.IMAGE_UPLOAD_FAILED })
     public ResponseEntity<ApiResponse<List<PresignedUrlResponse>>> getPresignedUrls(
             @RequestBody PresignedUrlRequest request
     ) {
