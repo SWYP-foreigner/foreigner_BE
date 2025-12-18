6b81c3971fc1666c8474e04a0c81ae9cb9663213
lixxce5017 <lixxce5017@gmail.com>
2025-12-18 20:46:23 +0900

    fix: 디바이스 토큰 등록 시 중복 데이터로 인한 서버 에러 해결
    DB에 동일한 device_token이 중복 저장된 경우, Optional 조회 시 IncorrectResultSizeDataAccessException 발생하여 500 에러 유발.
    
    [해결]
    
    Repository: findByDeviceToken의 반환 타입을 Optional에서 List로 변경하여 중복 데이터 조회 허용.
    
    Service: 토큰 등록(registerDeviceToken) 시 중복된 토큰이 조회되면, 첫 번째 데이터만 업데이트하고 나머지는 자동으로 삭제(Delete)하는 자가 치유(Self-healing) 로직 추가.

--- src/main/java/core/domain/notification/service/UserNotificationService.java
+++ src/main/java/core/domain/notification/service/UserNotificationService.java
@@ -9,6 +9,7 @@ import core.domain.userdevicetoken.entity.UserDeviceToken;
 import core.domain.userdevicetoken.repository.UserDeviceTokenRepository;
 import core.domain.usernotificationsetting.entity.UserNotificationSetting;
 import core.domain.usernotificationsetting.repository.UserNotificationSettingRepository;
+import core.global.enums.DeviceType;
 import core.global.enums.ImageType;
 import core.global.enums.NotificationType;
 import core.global.exception.BusinessException;
@@ -47,20 +48,25 @@ public class UserNotificationService {
     /**
      * FCM 기기 토큰을 등록하거나 갱신합니다. (람다 제거 버전)
      */
+    @Transactional // 삭제 로직이 들어가므로 트랜잭션 필수
     public void registerDeviceToken(Long userId, String deviceToken) {
         User user = findUserById(userId);
+        List<UserDeviceToken> tokens = userDeviceTokenRepository.findByDeviceToken(deviceToken);
 
-        Optional<UserDeviceToken> optionalToken = userDeviceTokenRepository.findByDeviceToken(deviceToken);
-
-        if (optionalToken.isPresent()) {
-            UserDeviceToken existingToken = optionalToken.get();
-            existingToken.updateUser(user);
-        } else {
+        if (tokens.isEmpty()) {
             UserDeviceToken newToken = new UserDeviceToken(user, deviceToken);
             userDeviceTokenRepository.save(newToken);
+
+        } else {
+            UserDeviceToken mainToken = tokens.get(0);
+            mainToken.updateUser(user);
+            if (tokens.size() > 1) {
+                for (int i = 1; i < tokens.size(); i++) {
+                    userDeviceTokenRepository.delete(tokens.get(i));
+                }
+            }
         }
     }
-
     /**
      * 사용자의 알림 설정 상태를 확인합니다. (이 메서드는 원래 람다를 사용하지 않았습니다)
      */
