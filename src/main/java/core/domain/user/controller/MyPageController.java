bac7a64254340813991782c50679b6268a157c3e
Lee Yongjun <97494494+usingjun@users.noreply.github.com>
2025-12-20 01:17:06 +0900

    Revert "Merge branch 'dev' into bugfix/459-following-status-오류-문제"

--- src/main/java/core/domain/user/controller/MyPageController.java
+++ src/main/java/core/domain/user/controller/MyPageController.java
@@ -129,7 +129,7 @@ public class MyPageController {
             Authentication authentication,
             @PathVariable("friendId") Long friendId) {
 
-        followService.unfollow(authentication, friendId);
+        followService.unfollowAccepted(authentication, friendId);
         featureUsageMetrics.recordFollowUsage();
 
         return ResponseEntity.ok().build();
