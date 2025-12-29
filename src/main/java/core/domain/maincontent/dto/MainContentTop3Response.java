47fa20b298d4483821ce76c99f2a6b0d77db3218
usingjun <usingjun99@gmail.com>
2025-12-29 17:20:47 +0900

    feat: response dto에 contentId, thumbImageUrl 제공

--- src/main/java/core/domain/maincontent/dto/MainContentTop3Response.java
+++ src/main/java/core/domain/maincontent/dto/MainContentTop3Response.java
@@ -3,9 +3,11 @@ package core.domain.maincontent.dto;
 import core.domain.maincontent.entity.KNewsContentType;
 
 public record MainContentTop3Response(
+        Long contentId,
         String title,
         String contentPreview,
         KNewsContentType type,
-        Integer ago
+        Long ago,
+        String thumbImageUrl
 ) {
 }
