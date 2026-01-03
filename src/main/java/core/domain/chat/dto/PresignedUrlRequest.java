abdc90adb047b4a22b6e36b7a657b30e71cca392
lixxce5017 <lixxce5017@gmail.com>
2026-01-03 19:43:18 +0900

    chore: 스웨거 요청 dto 중복으로 잘못표기되는 문제 해결

--- src/main/java/core/domain/chat/dto/PresignedUrlRequest.java
+++ src/main/java/core/domain/chat/dto/PresignedUrlRequest.java
@@ -1,3 +1,6 @@
 package core.domain.chat.dto;
 
-public record PresignedUrlRequest(String fileName) {}
+import io.swagger.v3.oas.annotations.media.Schema;
+
+public record ChatPresignedUrlRequest(
+        @Schema(description = "업로드할 파일명 (확장자 포함)", example = "image.png") String fileName) {}
