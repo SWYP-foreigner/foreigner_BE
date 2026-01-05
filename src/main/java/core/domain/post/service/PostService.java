5266c9476a75b307dd4068eb50b5da8ae6c02de1
user <dddsa1313@gmail.com>
2026-01-05 20:49:23 +0900

    feat: 어드민 메인 페이지 콘텐츠 작성 시 K-News 카테고리 분류 기능 추가

--- src/main/java/core/domain/post/service/PostService.java
+++ src/main/java/core/domain/post/service/PostService.java
@@ -40,7 +40,7 @@ public interface PostService {
     void blockPost(@Positive Long postId);
 
     void createAdminPost(String title, String content, String publishType,
-                         String boardCategoryStr,
+                         String boardCategoryStr, String kNewsTypeStr,
                          List<MultipartFile> generalImages,
                          MultipartFile mainThumbnailFile, MultipartFile popularThumbnailFile,
                          List<MultipartFile> contentImages,
