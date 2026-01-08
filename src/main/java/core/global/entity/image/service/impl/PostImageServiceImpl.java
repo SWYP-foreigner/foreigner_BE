d69ce7b36af74b24866ab6b58385ee4f853405d2
usingjun <usingjun99@gmail.com>
2026-01-08 22:29:46 +0900

    chore: post 경로 변경

--- src/main/java/core/global/entity/image/service/impl/PostImageServiceImpl.java
+++ src/main/java/core/global/entity/image/service/impl/PostImageServiceImpl.java
@@ -148,7 +148,7 @@ public class PostImageServiceImpl implements PostImageService {
             throw new BusinessException(ImageErrorCode.POST_IMAGES_ALREADY_EXIST);
         }
 
-        final String basePrefix = "test-posts/posts/" + postId;
+        final String basePrefix = "posts/" + postId;
 
         // 3) 병렬 COPY (스테이징 원본은 목록에 모아 한 번에 삭제)
         CopyResult copyResult = copyNewImagesInParallel(
@@ -192,7 +192,7 @@ public class PostImageServiceImpl implements PostImageService {
             return;
         }
 
-        final String basePrefix = "test-posts/posts/" + postId;
+        final String basePrefix = "posts/" + postId;
 
         // 3) 병렬 COPY (스테이징 원본은 목록에 모아 한 번에 삭제)
         CopyResult copyResult = copyNewImagesInParallel(
