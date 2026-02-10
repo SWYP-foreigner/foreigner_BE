6732b6740301e8b8e70163cd5858a320a2f8212a
lixxce5017 <lixxce5017@gmail.com>
2026-02-10 20:52:17 +0900

    chore: AppleAsnWebhookController 이름 복구

--- src/main/java/core/domain/payment/controller/AppleAsnWebhookController.java
+++ src/main/java/core/domain/payment/controller/AppleAsnWebhookController.java
@@ -11,7 +11,6 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api/iap/apple")
 public class AppleAsnWebhookController {
-
     private final AppleAsnWebhookService webhookService;
 
     public AppleAsnWebhookController(AppleAsnWebhookService webhookService) {
